982841c5315ece430acb44ee2088df0752c6c9cc
Angel <angelgmb2@hotmail.com>
2024-03-20 02:30:13 -0600

    Update code formatting

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'dull-lavender': {
+					'50': '#f6f3ff',
+					'100': '#efe9fe',
+					'200': '#e1d7fd',
+					'300': '#ccb6fc',
+					'400': '#b18bf8',
+					'500': '#9a5ff3',
+					'600': '#8e3dea',
+					'700': '#7e2bd6',
+					'800': '#6a24b3',
+					'900': '#571f93',
+					'950': '#361164',
+			},
+			}
+		},
+	},
+	plugins: [],
+}
